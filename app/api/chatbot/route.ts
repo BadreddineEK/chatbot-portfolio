b9be053e16be78a48218f1acf1553ff32a24b26c
Badreddine <bktec3@gmail.com>
2025-01-22 22:03:18 +0100

    ajout api

--- app/api/chatbot/route.ts
+++ app/api/chatbot/route.ts
@@ -7,7 +7,7 @@ export async function POST(request: Request) {
   // Configuration CORS pour autoriser les requêtes depuis ton portfolio GitHub Pages
   const response = NextResponse.json({ botResponse: 'This is a response from the bot!' });
 
-  response.headers.set('Access-Control-Allow-Origin', 'https://ton-portfolio-github-pages-url.com'); // Remplace par l'URL de ton portfolio
+  response.headers.set('Access-Control-Allow-Origin', 'https://badreddineek.github.io/Portfolio/index.html#'); // Remplace par l'URL de ton portfolio
   response.headers.set('Access-Control-Allow-Methods', 'POST');
   response.headers.set('Access-Control-Allow-Headers', 'Content-Type');
 
